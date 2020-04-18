7fcc491a15ae25df811ea6836c2ab78630154689
Marcelo Diament <marcelodiament@gmail.com>
2020-04-18 20:21:33 -0300

    página de pagamento completa

--- pagamento.php
+++ pagamento.php
@@ -3,14 +3,14 @@
 <main class="container-fluid">
     <header class="topo-pagina p-3 mb-5" id="topoPagamento">
         <h1 class="p-3">Pagamento</h1>
-        <p class="px-3 pt-0 topo">Entre em contato conosco e solicite uma condição especial para sua compra!</p>
+        <p class="px-3 pt-0 topo">Você está quase lá! Complete o formulário a seguir e depois é só aguardar!</p>
     </header>
     <section class="container my-3">
-        <div class="row">
-            <article class="form col-12 col-md-7" id="formularioContato">
-                <h4>Endereço de Entrega</h4>
-                <p class="ml-0">Preencha o formulário formulário com os dados do endeço de entrega</p>
-                <form action="" method="post">
+        <form action="pagamento-confirmado.php" method="post" class="row">
+            <article class="col-12 col-md-7" id="formularioPagamento">
+                <div class="sticky-top">
+                    <h4>Endereço de Entrega</h4>
+                    <p class="ml-0">Preencha o formulário formulário com os dados do endeço de entrega</p>
                     <div class="form-row">
                         <div class="form-group col-md-6">
                             <label for="nomeDestinatario">Nome do Destinatário</label>
@@ -65,40 +65,72 @@
                     </div>
                     <div class="form-group">
                         <div class="form-check">
-                            <input class="form-check-input" type="checkbox" name="optIn-newsletter" id="optIn-newsletter" checked>
-                            <label class="form-check-label" for="optIn-newsletter">Por favor, me envie promoções!</label>
+                            <input class="form-check-input" type="checkbox" name="enderecoEntregaIgualDeCobranca" id="enderecoEntregaIgualDeCobranca" checked>
+                            <label class="form-check-label" for="enderecoEntregaIgualDeCobranca">O endereço de entrega é o mesmo que o de cobrança</label>
                         </div>
                     </div>
-                    <div class="form-group clearfix">
-                        <button type="submit" class="btn btn-info float-right">Enviar</button>
-                    </div>
-                </form>
+                </div>
             </article>
-            <article class="dados-contato col-12 col-md-5" id="dadosContato">
-                <h4>Entre em contato conosco!</h4>
-                <p class="ml-0">Se preferir, entre em contato conosco - teremos prazer em te ajudar!</p>
-                <ul class="mt-5 p-4 rounded bg-light">
-                    <li class="mt-3 mb-5 horario">
-                        <h5 class="mb-2 text-center">Horário de Funcionamento</h5>
-                        <p class="text-center">Segunda à Sábado das 8h às 18h</p>
+            <article class="dados-pedido col-12 col-md-5" id="dadosPedido">
+                <h4>Dados do Pedido</h4>
+                <p class="ml-0">Confirmação do Pedido</p>
+                <ul>
+                    <li class="mb-1">
+                        <p class="mb-0"><span class="checkout-qtd">1</span> Produto 04 | M | <span class="checkout-price">R$ 99.99</span></p>
+                        <small class="text-muted mt-0">SKU: 123-EFIUAB-98</small>
                     </li>
-                    <h5 class="mb-3 text-center">Meios de Contato</h5>
-                    <li class="text-center"><a href="https://wa.me/+5511976052723?text=Ol%C3%A1,%0D%0A%0D%0AAcessei%20o%20site%20e%20gostaria%20de%20saber%20mais%20sobre%0D%0A" title="Envie um zap pra gente!"><i class="fab fa-whatsapp mr-2"></i>11 97605 2723</a></li>
-                    <li class="text-center"><a href="tel:+5511976052723" title="Ligue para a gente"><i class="fas fa-phone mr-2"></i>11 2345 6789</a></li>
-                    <li class="text-center"><a href="mailto:contato@djament.com.br" title="Envie um email para a gente"><i class="fas fa-envelope mr-2"></i>marcelo@djament.com.br</a></li>
-                    <li class="text-center"><a href="https://g.page/digitalhousebrasil?share" target="_blank" title="Veja nosso endereço no Google Maps"><i class="fa fa-map-marker-alt mr-2"></i>Av. Dr. Cardoso de Melo, 90 - Vila Olímpia, São Paulo</a></li>
-                    <li class="mt-3 mb-5 redes-sociais">
-                        <h5 class="mb-4 text-center">Conecte-se com a gente</h5>
-                        <div class="d-flex flex-row flex-nowrap justify-content-center">
-                            <a href="#" target="_blank" title="Acesse nosso Insta"><i class="fab fa-instagram mr-2"></i></a>
-                            <a href="#" target="_blank" title="Acesse nosso Face"><i class="fab fa-facebook mr-2"></i></a>
-                            <a href="#" target="_blank" title="Acesse nosso Twitter"><i class="fab fa-twitter mr-2"></i></a>
-                            <a href="#" target="_blank" title="Acesse nosso Pinterest"><i class="fab fa-pinterest mr-2"></i></a>
-                        </div>
+                    <li class="mb-1">
+                        <p class="mb-0"><span class="checkout-qtd">2</span> Produto 07 | GG | <span class="checkout-price">R$ 199.98</span></p>
+                        <small class="text-muted mt-0">SKU: 103-EF45EF-98</small>
+                    </li>
+                    <li class="mb-1">
+                        <p class="mb-0"><span class="checkout-qtd">1</span> Produto 08 | PP | <span class="checkout-price">R$ 99.99</span></p>
+                        <small class="text-muted mt-0">SKU: 003-RWIUAB-66</small>
+                    </li>
+                    <hr class="text-muted">
+                    <li class="mb-1">
+                        <p class="mb-0">Subtotal <span class="checkout-subtotal-price text-muted font-weight-bold">R$ 399.96</span></p>
+                    </li>
+                    <li class="mb-1">
+                        <p class="mb-0">Cupom de desconto <span class="checkout-cupom text-info font-weight-bold">- R$ 100.00</span></p>
+                    </li>
+                    <li class="mb-1">
+                        <p class="mb-0">Frete <span class="checkout-frete-price text-muted font-weight-bold">R$ 50.00</span></p>
+                        <small class="text-muted mt-0">Prazo de Entrega: até 5 dias úteis</small>
+                    </li>
+                    <hr class="bg-info">
+                    <li class="mb-1">
+                        <p class="mb-0"><b>Total <span class="checkout-total-price">R$ 349.96</span></b></p>
                     </li>
                 </ul>
+                <h4 class="mt-5">Dados de Pagamento</h4>
+                <p class="ml-0">Preencha os dados de pagamento a seguir</p>
+                <div class="form-row">
+                    <div class="form-group col-12">
+                        <label for="nomeTitular">Nome do Titular do Cartão</label>
+                        <input type="text" class="form-control" name="nomeTitular" id="nomeTitular" placeholder="Nome Completo" aria-describedby="#nomeTitularHelp" required>
+                        <small id="nomeTitularHelp" class="form-text text-muted">Digite o nome exatamente como está no cartão</small>
+                    </div>
+                </div>
+                <div class="form-row">
+                    <div class="form-group col-md-5">
+                        <label for="numeroCartao">Número do Cartão</label>
+                        <input type="text" class="form-control" name="numeroCartao" id="numeroCartao" placeholder="1234 5678 9012 3456" required>
+                    </div>
+                    <div class="form-group col-md-2">
+                        <label for="cvcCartao">Cód.</label>
+                        <input type="number" class="form-control" name="cvcCartao" id="cvcCartao" placeholder="123" required>
+                    </div>
+                    <div class="form-group col-md-5">
+                        <label for="vencimentoCartao">Vencimento</label>
+                        <input type="month" class="form-control" name="vencimentoCartao" id="vencimentoCartao" placeholder="123" required>
+                    </div>
+                </div>
+                <div class="form-group clearfix">
+                    <button type="submit" class="btn btn-info float-right">Confirmar Pagamento</button>
+                </div>
             </article>
-        </div>
+        </form>
     </section>
 </main>
 <?php require_once("./inc/footer.php"); ?>
